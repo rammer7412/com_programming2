238c319a32aefe3b0354323b601916835fd7cd6b
rammer7412 <coderammer7412@gmail.com>
2024-11-26 21:06:25 +0900

    gamma 1st commit
    Add some part of module (import, define var names, plt, and so on)

--- Gamma.py
+++ Gamma.py
@@ -1,7 +1,13 @@
-from matplotlib.pyplot import plt
-
-
-
+import matplotlib.pyplot as plt
+import pandas as pd
+import numpy as np
+data_men=[]
+data_women=[]
+data_len_men=len(data_men)
+data_len_women=len(data_women)
+plt.plot(data_men,'k--')
+plt.plot(data_women,'k--')
+plt.show()
 
 if __name__ == "__main__":
     print("이 모듈은 직접 실행되었습니다.")
