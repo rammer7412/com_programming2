9f0fbe5112d4a9aeca09c837610e1f8091934838
owl-91391 <ljw91391@naver.com>
2024-11-30 00:22:32 +0900

    수정3

--- Alpha.py
+++ Alpha.py
@@ -3,7 +3,7 @@ import Gamma
 
 data = Beta.get_data()
 subject = list(data.keys())[1:]
-print('<과목명>')
+
 print(f'{data["year"]}학년도 과목명>')
 for i in range(len(subject)):
     print(f'{i + 1}. {subject[i]}')
