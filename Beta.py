a798ee2b5f844f730e50a8bf079d79eb66c48c49
owl-91391 <ljw91391@naver.com>
2024-11-30 13:06:39 +0900

    beta 조금 수정

--- Beta.py
+++ Beta.py
@@ -4,33 +4,15 @@ data = pd.read_csv('20231231_utf8bom.csv')
 
 data_preprocessing = {}
 data_preprocessing['year'] = 2024
+check = set()
 
- 
-data_preprocessing['국어'] = data.loc[data['영역'] == "국어", ["표준점수","남자","여자"]]
-data_preprocessing['수학'] = data.loc[data['영역'] == "수학", ["표준점수","남자","여자"]]
-data_preprocessing['생활과 윤리'] = data.loc[data['유형'] == "생활과 윤리", ["표준점수","남자","여자"]]
-data_preprocessing['윤리와 사상'] = data.loc[data['유형'] == "윤리와 사상", ["표준점수","남자","여자"]]
-data_preprocessing['한국지리'] = data.loc[data['유형'] == "한국지리", ["표준점수","남자","여자"]]
-data_preprocessing['세계지리'] = data.loc[data['유형'] == "세계지리", ["표준점수","남자","여자"]]
-data_preprocessing['동아시아사'] = data.loc[data['유형'] == "동아시아사", ["표준점수","남자","여자"]]
-data_preprocessing['세계사'] = data.loc[data['유형'] == "세계사", ["표준점수","남자","여자"]]
-data_preprocessing['경제'] = data.loc[data['유형'] == "경제", ["표준점수","남자","여자"]]
-data_preprocessing['사회·문화'] = data.loc[data['유형'] == "사회·문화", ["표준점수","남자","여자"]]
-data_preprocessing['물리학 I'] = data.loc[data['유형'] == "물리학 I", ["표준점수","남자","여자"]]
-data_preprocessing['화학 I'] = data.loc[data['유형'] == "화학 I", ["표준점수","남자","여자"]]
-data_preprocessing['생명과학 I'] = data.loc[data['유형'] == "생명과학 I", ["표준점수","남자","여자"]]
-data_preprocessing['지구과학 I'] = data.loc[data['유형'] == "지구과학 I", ["표준점수","남자","여자"]]
-data_preprocessing['물리학 II'] = data.loc[data['유형'] == "물리학 II", ["표준점수","남자","여자"]]
-data_preprocessing['화학 II'] = data.loc[data['유형'] == "화학 II", ["표준점수","남자","여자"]]
-data_preprocessing['생명과학 II'] = data.loc[data['유형'] == "생명과학 II", ["표준점수","남자","여자"]]
-data_preprocessing['지구과학 II'] = data.loc[data['유형'] == "지구과학 II", ["표준점수","남자","여자"]]
-data_preprocessing['성공적인 직업생활'] = data.loc[data['유형'] == "성공적인 직업생활", ["표준점수","남자","여자"]]
-data_preprocessing['농업 기초 기술'] = data.loc[data['유형'] == "농업 기초 기술", ["표준점수","남자","여자"]]
-data_preprocessing['공업 일반'] = data.loc[data['유형'] == "공업 일반", ["표준점수","남자","여자"]]
-data_preprocessing['상업 경제'] = data.loc[data['유형'] == "상업 경제", ["표준점수","남자","여자"]]
-data_preprocessing['수산·해운 산업 기초'] = data.loc[data['유형'] == "수산·해운 산업 기초", ["표준점수","남자","여자"]]
-data_preprocessing['인간 발달'] = data.loc[data['유형'] == "인간 발달", ["표준점수","남자","여자"]]
-
+for subjects in data['유형'] :
+    if subjects in check :
+        continue
+    else :
+        data_preprocessing[subjects] = data.loc[data['유형'] == subjects, ["표준점수","남자","여자"]]
+        check.add(subjects)
+        
 def get_data() :
     return data_preprocessing
 
